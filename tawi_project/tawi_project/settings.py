4501d79206e97f4eb5949e9f95ed020f4bac9b8a
mahuthu <mahuthup@gmail.com>
2023-11-25 00:10:36 +0300

    version2

--- tawi_project/tawi_project/settings.py
+++ tawi_project/tawi_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-l8@13g=vdx&jl3zd#)sju^(_=dh2$=+cu-z20-x7yn=8-oc51j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','savannah-ai.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','social-media-app-7gez.onrender.com']
 
 
 
